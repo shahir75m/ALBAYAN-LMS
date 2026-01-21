490e3d3363637b4c3eb46ce466c74f9cae949e57
shahir75m <shahirzaman75@gmail.com>
2026-01-21 09:13:00 +0530

    UI: Final definitive logo overhaul for a professional 'neat' finish

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,20 +9,25 @@ const Logo: React.FC<LogoProps> = ({ className = "w-10 h-10" }) => {
   return (
     <svg
       viewBox="0 0 24 24"
-      fill="currentColor"
-      xmlns="http://www.w3.org/2000/svg"
       className={className}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M12 21V7C12 7 10 5 4 5V17C8 17 12 19 12 19C12 19 16 17 20 17V5C14 5 12 7 12 7V21Z"
-        opacity="0.95"
+        d="M2 3H8C9.06087 3 10.0783 3.42143 10.8284 4.17157C11.5786 4.92172 12 5.93913 12 7V21C12 20.4696 11.7893 19.9609 11.4142 19.5858C11.0391 19.2107 10.5304 19 10 19H2V3Z"
+        fill="currentColor"
+        fillOpacity="0.1"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinejoin="round"
       />
-      <path d="M6 8H9" fill="white" fillOpacity="0.2" />
-      <path d="M6 11H9" fill="white" fillOpacity="0.2" />
-      <path d="M15 8H18" fill="white" fillOpacity="0.2" />
-      <path d="M15 11H18" fill="white" fillOpacity="0.2" />
+      <path
+        d="M22 3H16C14.9391 3 13.9217 3.42143 13.1716 4.17157C12.4214 4.92172 12 5.93913 12 7V21C12 20.4696 12.2107 19.9609 12.5858 19.5858C12.9609 19.2107 13.4696 19 14 19H22V3Z"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
